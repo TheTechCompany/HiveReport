9cf0932be00833be88cd1f434117a97f5a98fff9
Ross Leitch <ross@hexhive.io>
2022-05-09 11:52:21 +1200

    Added init graphgrid

--- packages/app/hivereport-frontend/webpack.config.js
+++ packages/app/hivereport-frontend/webpack.config.js
@@ -12,7 +12,16 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          resolve: {
+              fullySpecified: false,
+          },
+        }
+      ]
+    },
     plugins: [
       new webpack.ProvidePlugin({
         process: 'process/browser',
